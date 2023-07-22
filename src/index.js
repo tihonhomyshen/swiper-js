b52ccb1d8e69682b72b6891ecc52fa87b0bbee33
tihonhomyshen <lilo.frich@gmail.com>
2023-07-22 17:10:39 +0300

    fraction type

---
+++ src/index.js
@@ -0,0 +1,18 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        // clickable: true,
+        // dynamicBullets: true,
+
+        // renderBullet: function (index, className){
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+        type: 'fraction',
+    },
+});
+
